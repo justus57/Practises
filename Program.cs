6651aa321cff24d59468278a24be51427e4b4e81
Joseph K Justus <justuskasyoki57@gmail.com>
2022-04-10 11:26:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 10, 30, 15 };
+            int x = 30;
+            int n = arr.Length;
+            Console.WriteLine(x + "is present at index " + search(arr, n, x));
+            Console.ReadLine();
+            
+        }
+        static int search(int[] arr, int n,int x)
+        {
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                if (arr[i] == x)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
