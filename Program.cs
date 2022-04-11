21da5a542d506e5b6f09e73e8cc2430872de8e4e
Joseph K Justus <justuskasyoki57@gmail.com>
2022-04-11 05:22:00 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -14,8 +14,19 @@ namespace Practises
             int x = 30;
             int n = arr.Length;
             Console.WriteLine(x + "is present at index " + search(arr, n, x));
-            Console.ReadLine();
+         
+
+            node start = new node("d");
+            start.next = new node("d");
+            start.next.next = new node("e");
+            printList(start);
+            removeduplicates(start);
+            Console.WriteLine("final result");
+            printList(start);
             
+            Console.ReadLine();
+
+
         }
         static int search(int[] arr, int n,int x)
         {
@@ -29,5 +40,45 @@ namespace Practises
             }
             return -1;
         }
+        public static void removeduplicates(node head)
+        {
+            HashSet<string> hs = new HashSet<string>();
+            node current = head;
+            node prev = null;
+            while(current != null)
+            {
+                string curval = current.val;
+                if (hs.Contains(curval))
+                {
+                    prev.next = current.next;
+                }
+                else
+                {
+                    hs.Add(curval);
+                    prev = current;
+                }
+                current = current.next;
+            }
+        }
+        public class node
+        {
+            public string val;
+            public node next;
+            public node(string val)
+            {
+                this.val = val;
+            }
+        }
+
+        //function to print nodes in a given linked list
+        public static void printList(node head)
+        {
+            while(head != null)
+            {
+                Console.WriteLine(head.val + " ");
+                head = head.next;
+            }
+        }
+
     }
 }
